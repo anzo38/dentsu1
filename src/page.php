54f9e90042ffc1bb8e56edf3dc653aa341e21a74
zoey <anzo.sa8@gmail.com>
2021-03-09 23:53:33 +0900

    親クラス作成

---
+++ src/page.php
@@ -0,0 +1,22 @@
+<?php
+class Page {
+
+  private static $singleton;
+  private static $smarty=null;
+
+  public static function smarty(){
+    $this->smarty = new Smarty();
+    $this->smarty->template_dir = './templates/';
+    $this->smarty->compile_dir  = './templates_c/';
+    $this->smarty->setConfigDir('./configs/');
+    $this->smarty->configLoad('const.conf');
+    if (!isset(self::$singleton)) {
+      self::$singleton = new Page();    
+  }
+
+  return self::$singleton;
+  }
+
+
+
+}
